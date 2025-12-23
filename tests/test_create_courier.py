65ea81bddce031501a0a5e9d5ac351f8ccf7b2bd
qaingeener-create <qaingeener@gmail.com>
2025-12-23 19:20:06 +0300

    шаги  вынес в отдельный модуль

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -4,25 +4,27 @@ from data.urls import url
 from data.courier_data import generation_new_data_courier
 import logging
 
+# Вынесем шаги в отдельный модуль
+def create_courier(payload):
+    response = requests.post(f"{url}/api/v1/courier", json=payload)
+    return response
 
+def login_courier(login_payload):
+    response = requests.post(f"{url}/api/v1/courier/login", json=login_payload)
+    return response
 
-
+def delete_courier(courier_id):
+    response = requests.delete(f"{url}/api/v1/courier/{courier_id}")
+    return response
 
 class TestCreateCourier:
-
-    @allure.step('Создание курьера')
-    def create_courier(payload):
-            response = requests.post(f"{url}/api/v1/courier", data=payload)
-            return response
-
-    def test_create_courier(self, registered_courier_data):
+    @allure.title('Создание курьера')
+    def test_create_courier(self):
         data = generation_new_data_courier()
-        data.pop("firstName")
         payload = data
         logging.info(f"Data for courier creation: {data}")
-        
 
-        response = requests.post(f"{url}/api/v1/courier", data=payload)
+        response = create_courier(payload)
         assert response.status_code == 201
         assert response.json() == {"ok": True}, "Неверное содержимое ответа."
 
@@ -30,34 +32,27 @@ class TestCreateCourier:
             "login": payload["login"],
             "password": payload["password"]
         }
-        login_response = requests.post(f"{url}/api/v1/courier/login", data=login_payload)
+        login_response = login_courier(login_payload)
         assert login_response.status_code == 200, "Login failed."
 
         courier_id = login_response.json().get("id")
         assert courier_id is not None, "Courier ID not found in login response."
 
         # Delete the created courier
-        delete_response = requests.delete(f"{url}/api/v1/courier/{courier_id}")
+        delete_response = delete_courier(courier_id)
         assert delete_response.status_code == 200, "Failed to delete courier."
 
-    @allure.step('Проверка создания курьера с уже существующими учётными данными')
-    def create_courier_with_duplicate_credentials(payload):
-        response = requests.post(f"{url}/api/v1/courier", data=payload)
-        return response
-    @allure.title('Проверка невозможности создать курьера. дублирующие креды')
-    @allure.description('Проверка, что нельзя создать курьера с уже существующеми кредами (код - 409 и текст - "message": "Этот логин уже используетсяПопробуйте другой."')
-    def test_create_courier_duplicate_login(self, registered_courier_data):
-        payload = registered_courier_data
-        response = requests.post(f"{url}/api/v1/courier", data=payload)
+    @allure.title('Проверка невозможности создать курьера с уже существующими учётными данными')
+    @allure.description('Проверка, что нельзя создать курьера с уже существующеми кредами (код - 409 и текст - "message": "Этот логин уже используется. Попробуйте другой.")')
+    def test_create_courier_duplicate_login(self):
+        data = generation_new_data_courier()
+        payload = data
+        response = create_courier(payload)
 
         assert response.status_code == 409
         assert response.json() == {"code": 409, "message": "Этот логин уже используется. Попробуйте другой."}, "Неверное содержимое ответа."
 
-    @allure.step('Проверка создания курьера без всех обязательных полей')
-    def create_courier_without_required_fields(payload):
-        response = requests.post(f"{url}/api/v1/courier", data=payload)
-        return response
-    @allure.title('Проверка невозможности создать курьера. Не все обязательные поля')
+    @allure.title('Проверка невозможности создать курьера без всех обязательных полей')
     @allure.description(
         'Проверка заполнения не всех обязательных полей. Курьер не создан (код - 400 и текст - "message": "Недостаточно данных для создания учетной записи"')
     def test_create_courier_without_password(self):
@@ -66,7 +61,7 @@ class TestCreateCourier:
             "login": data["login"],
             "firstName": data["firstName"]
         }
-        response = requests.post(f"{url}/api/v1/courier", data=payload)
+        response = requests.post(f"{url}/api/v1/courier", json=payload)
 
         assert response.status_code == 400
         assert response.json() == {"code": 400,
