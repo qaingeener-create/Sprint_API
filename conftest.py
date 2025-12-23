8ac5e0a0b7d1f407dab55f2dc3f7a29daa1d7147
qaingeener-create <qaingeener@gmail.com>
2025-12-23 20:44:04 +0300

    фикстуры

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from data.courier_data import register_new_courier_and_return_login_password
 import logging
+import requests
+from data.urls import url
 
 @pytest.fixture
 def registered_courier_data():
@@ -10,3 +12,16 @@ def registered_courier_data():
         "password": login_pass[1],
         "firstName": login_pass[2]
     }
+
+@pytest.fixture
+def delete_courier_data():
+    login_pass = register_new_courier_and_return_login_password()
+    yield {
+        "login": login_pass[0],
+        "password": login_pass[1]
+    }
+    response = requests.post(f"{url}/login_courier_endpoint", data=login_pass)
+    if response.status_code == 200:
+        courier_id = response.json().get("id")
+        if courier_id:
+            requests.delete(f"{url}/create_courier_endpoint/{courier_id}")
--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -4,18 +4,7 @@ import allure
 from data.urls import url
 from data.courier_data import register_new_courier_and_return_login_password
 
-@pytest.fixture
-def delete_courier_data():
-    login_pass = register_new_courier_and_return_login_password()
-    yield {
-        "login": login_pass[0],
-        "password": login_pass[1]
-    }
-    response = requests.post(f"{url}/api/v1/courier/login", data=login_pass)
-    if response.status_code == 200:
-        courier_id = response.json().get("id")
-        if courier_id:
-            requests.delete(f"{url}/api/v1/courier/{courier_id}")
+
 
 
 class TestLoginCourier:
