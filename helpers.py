a1227e6e13e6b289499a05165f03a7f9a068f9a0
qaingeener-create <qaingeener@gmail.com>
2025-12-25 19:47:51 +0300

    step

--- helpers.py
+++ helpers.py
@@ -30,3 +30,18 @@ def register_new_courier_and_return_login_password():
 
     if response.status_code == 201:
         return [data["login"], data["password"], data["firstName"]]
+
+@step('Create courier')
+def create_courier(payload):
+    response = requests.post(f"{url}/api/v1/courier", json=payload)
+    return response
+
+@step('Login courier')
+def login_courier(login_payload):
+    response = requests.post(f"{url}/api/v1/courier/login", json=login_payload)
+    return response
+
+@step('Delete courier')
+def delete_courier(courier_id):
+    response = requests.delete(f"{url}/api/v1/courier/{courier_id}")
+    return response
--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -4,18 +4,7 @@ from data.urls import url
 from data.courier_data import generation_new_data_courier
 import logging
 
-# Вынесем шаги в отдельный модуль
-def create_courier(payload):
-    response = requests.post(f"create_courier_url", json=payload)
-    return response
 
-def login_courier(login_payload):
-    response = requests.post(f"create_courier_url", json=login_payload)
-    return response
-
-def delete_courier(courier_id):
-    response = requests.delete(f"create_courier_url/{courier_id}")
-    return response
 
 class TestCreateCourier:
     @allure.title('Создание курьера')
